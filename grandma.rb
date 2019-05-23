6bdb736adecdb60d0d57d4142ce1e118810e986a
Kelly Dobbins <kellydobbins26@outlook.com>
2019-05-23 19:16:18 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,12 @@
-def speak_to_grandma 
+def speak_to_grandma(phrase)
 
-if phrase = "I LOVE YOU GRANDMA!"
-  puts "I LOVE YOU TOO PUMPKIN!"
-  
-else phrase = "".lowercase
-  puts "HUH!? SPEAK UP SONNY!"
-  
-  elsif phrase = "".uppercase
-  puts "NO, NOT SINCE 1938!"
+if phrase == "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
+end
+
+]if phrase != "".upcase()
+  return "HUH!? SPEAK UP SONNY!"
+else 
+  return "NO, NOT SINCE 1938!"
+end
 end
